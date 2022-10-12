73cbcf0e230551c8c295d18c273d4df0ddef36bd
Lucía Manso-Ortega <l.manso@bcbl.eu>
2022-10-12 19:23:56 +0200

    Update P01_Unzip.m

--- P01_Unzip.m
+++ P01_Unzip.m
@@ -1,30 +1,29 @@
 %% Unzip images to nii file
 
-%Authors: Lucía Manso & Laura de Frutos-Sagastuy. 
-%Basque Center on Cognition, Brain and Language (BCBL), San Sebastian, Spain
+%Authors: LucÃ­a Manso & Laura de Frutos-Sagastuy at the Basque Center on Cognition, Brain and Language (BCBL), San Sebastian, Spain
 %Last modification date: 04/03/2022
 %Content: Code for unziping files (.nii.gz) to .nii before working with them in SPM
-%This script has been done in Matlab 2021b.
+%This script has been done in Matlab 2021b. Note that not all Matlab versions have 'niftiread' available
+%Requisite before starting: Save a .txt file with the list of all the images you want to unzip
 
 %% Starting 
 clear all 
 clc
 
 % Define the working paths
-% Folder where the nii.gz files are
-input_folder = '';
-% Folder where you want to save the converted nii.gz
-output_folder = '';
+input_folder = ''; % Folder where the nii.gz files are
+output_folder = ''; % Folder where you want to save the converted nii.gz
 
 % Considering you have all files in one folder, create a .txt list with the exact name of the files
 % you want to convert from nii.gz to .nii.
-% Go to the input folder
-cd(input_folder);
+
+cd(input_folder); % Go to the input folder
+
 % Define the list. For this you need to have a list created in a txt file with all the files
 %in the input_folder. Each row must be one file name.
 input_list = textread('.txt','%s');
-% Go to where the files are converted
-cd(output_folder);
+cd(output_folder); % Go to where the files are converted
+
 % Unzip all files from the list
 gunzip(input_list)
 
